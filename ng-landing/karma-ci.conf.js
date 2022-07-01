8eec1d202b068c9bb0783fcb319cb5d23fbd8111
Peter Jokumsen <pjokumsen@gmail.com>
2022-07-01 23:40:32 +0200

    feat: add script `pr-ci` (#2)

--- ng-landing/karma-ci.conf.js
+++ ng-landing/karma-ci.conf.js
@@ -25,14 +25,13 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ng-landing'),
+      dir: require('path').join(__dirname, './coverage'),
       subdir: '.',
       reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
+        { type: 'json-summary' }
       ]
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
