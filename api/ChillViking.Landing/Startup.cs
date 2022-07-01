033ad5787c68ccb952db2aefc78293db5f2c7aa4
Peter Jokumsen <pjokumsen@gmail.com>
2022-07-02 01:18:04 +0200

    feat: add ChillViking.Landing for API

---
+++ api/ChillViking.Landing/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(ChillViking.Landing.Startup))]
+
+namespace ChillViking.Landing;
+
+public class Startup : FunctionsStartup
+{
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+        // nothing to do here for now.
+    }
+}
