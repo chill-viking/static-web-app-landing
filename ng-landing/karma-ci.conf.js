44738f5e66b3e3ba98bd131534cd2ccd67b25cfc
Peter Jokumsen <pjokumsen@gmail.com>
2022-07-02 14:20:52 +0200

    fix: update ci coverage report

--- ng-landing/karma-ci.conf.js
+++ ng-landing/karma-ci.conf.js
@@ -28,7 +28,7 @@ module.exports = function (config) {
       dir: require('path').join(__dirname, './coverage'),
       subdir: '.',
       reporters: [
-        { type: 'json-summary' }
+        { type: 'cobertura' }
       ]
     },
     reporters: ['kjhtml'],
