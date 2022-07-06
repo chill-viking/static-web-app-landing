d9b07b75e33faf908154bdaf1f5a794584fca9bd
Peter Jokumsen <pjokumsen@gmail.com>
2022-07-06 21:45:52 +0200

    fix: clear up code smells

--- ng-landing/src/app/about-us/about-us.component.ts
+++ ng-landing/src/app/about-us/about-us.component.ts
@@ -8,7 +8,6 @@ import {
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
-  styleUrls: ['./about-us.component.scss']
 })
 export class AboutUsComponent extends HasSlug {
   slug = 'about-us';
--- ng-landing/src/app/page-not-found/page-not-found.component.ts
+++ ng-landing/src/app/page-not-found/page-not-found.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {
+  MonitoringService,
+} from '@shared/services';
 
 @Component({
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.scss']
 })
-export class PageNotFoundComponent {
-  constructor() { }
+export class PageNotFoundComponent implements OnInit {
+  constructor(
+    private _logger: MonitoringService,
+    private _router: Router,
+  ) { }
+
+  ngOnInit(): void {
+    this._logger.logException({
+      message: `User landed on '${this._router.url}' somehow.`,
+      name: 'PageNotFound',
+    })
+  }
 }
