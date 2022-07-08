4be1c3f4646528a42bd57321b48b4c0f1fa28fea
Peter Jokumsen <pjokumsen@gmail.com>
2022-07-08 19:05:29 +0200

    feat: add @angular/material

--- ng-landing/src/app/app.module.ts
+++ ng-landing/src/app/app.module.ts
@@ -6,6 +6,9 @@ import {
   BrowserModule,
 } from '@angular/platform-browser';
 import {
+  BrowserAnimationsModule,
+} from '@angular/platform-browser/animations';
+import {
   SharedModule,
 } from '@shared/shared.module';
 import {
@@ -22,6 +25,7 @@ import {
     AppRoutingModule,
     HttpClientModule,
     SharedModule,
+    BrowserAnimationsModule,
   ],
   declarations: [
     AppComponent,
