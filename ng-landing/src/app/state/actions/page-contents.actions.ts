861d07bcbe9aeb05f9376b3072e762ad36554bab
Peter Jokumsen <pjokumsen@gmail.com>
2022-07-19 15:08:22 +0200

    feat: include action update

--- ng-landing/src/app/state/actions/page-contents.actions.ts
+++ ng-landing/src/app/state/actions/page-contents.actions.ts
@@ -2,11 +2,15 @@ import { createAction, props } from '@ngrx/store';
 import { PageContents } from '@shared/models';
 
 export const loadPageContents = createAction(
-  '[PageContents] Load PageContentss',
+  '[PageContents] Load PageContents',
+);
+
+export const pageContentsAlreadyLoaded = createAction(
+  '[PageContents] Load PageContents',
   props<{ slug: string }>(),
 );
 
 export const loadPageContentsSuccess = createAction(
-  '[PageContents] Load PageContentss Success',
+  '[PageContents] Load PageContents Success',
   props<{ data: PageContents, slug: string }>(),
 );
