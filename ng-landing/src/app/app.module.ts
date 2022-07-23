53a970f772ee8dded78d68ff74ac756b1b2537cd
Peter Jokumsen <pjokumsen@gmail.com>
2022-07-23 12:57:07 +0200

    chore: clean up app.module imports a bit

--- ng-landing/src/app/app.module.ts
+++ ng-landing/src/app/app.module.ts
@@ -1,25 +1,7 @@
 import {
-  LayoutModule,
-} from '@angular/cdk/layout';
-import {
   HttpClientModule,
 } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import {
-  MatButtonModule,
-} from '@angular/material/button';
-import {
-  MatIconModule,
-} from '@angular/material/icon';
-import {
-  MatListModule,
-} from '@angular/material/list';
-import {
-  MatSidenavModule,
-} from '@angular/material/sidenav';
-import {
-  MatToolbarModule,
-} from '@angular/material/toolbar';
 import {
   BrowserModule,
 } from '@angular/platform-browser';
@@ -44,13 +26,13 @@ import {
 import {
   AppRoutingModule,
 } from './app-routing.module';
-import { AppComponent } from './app.component';
 import {
-  PageNotFoundComponent,
-} from './page-not-found/page-not-found.component';
-import {
+  AppComponent, PageNotFoundComponent,
   PrimaryNavigationComponent,
-} from './primary-navigation/primary-navigation.component';
+} from './components';
+import {
+  MaterialModule,
+} from './material/material.module';
 import {
   NavigationMenuEffects, PageContentsEffects,
 } from './state/effects';
@@ -65,12 +47,7 @@ import {
     HttpClientModule,
     SharedModule,
     BrowserAnimationsModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
+    MaterialModule,
     StoreModule.forRoot(reducers, { metaReducers }),
     !environment.production ? StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }) : [],
     EffectsModule.forRoot([
---
+++ ng-landing/src/app/components.ts
@@ -0,0 +1,3 @@
+export * from './app.component';
+export * from './page-not-found/page-not-found.component';
+export * from './primary-navigation/primary-navigation.component';
